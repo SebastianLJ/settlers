84893f4dfd900f810f53b86d722c90a3564dc4e3
Emil_HomePC <emilsd.1996@gmail.com>
2020-01-13 15:48:56 +0100

    "Project Structure optimized for JavaFX"

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,34 @@
 package view;
 
-public class View {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class View extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        primaryStage.setTitle("Polygon Example");
+
+
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(0.0, 0.0,
+                200.0, 200.0,
+                200.0, 100.0);
+
+        root.getChildren().add(polygon);
+        Scene scene = new Scene(root,300,400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
