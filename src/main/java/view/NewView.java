9d943d270e83d18c65dd532e5ca0f9316ee8e488
Emil_HomePC <emilsd.1996@gmail.com>
2020-01-15 15:41:15 +0100

    "Update method that updates robber"

--- src/main/java/view/NewView.java
+++ src/main/java/view/NewView.java
@@ -1,37 +1,19 @@
 package view;
 
 import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import model.Game;
 import model.board.*;
 import model.newGame;
 
 public class NewView {
 
-    public static final int PLAYER_ONE = 0;
-    public static final int PLAYER_TWO = 1;
-    public static final int PLAYER_THREE = 2;
-    public static final int PLAYER_FOUR = 3;
-    private final int screenSize = 1000;
-
-    private final int HEX_SIZE = screenSize/10;
-    private final double HEX_WIDTH = Math.sqrt(3)*HEX_SIZE;
-    private final double HEX_HEIGHT = 2*HEX_SIZE;
-
-    // Coordinates for center tiles
-    private final int centerTileX = 2, centerTileY = 2;
-
-    // Offset calculated from the center tile
-    private final double centerCoordX = HEX_WIDTH*centerTileX + 0.5*HEX_WIDTH*centerTileY, centerCoordY = 3*HEX_HEIGHT/4*centerTileY;
-
-    private final double offsetX = screenSize/2.0 - centerCoordX, offsetY = screenSize/2.0 - centerCoordY;
-
+    private static final int PLAYER_ONE = 0;
+    private static final int PLAYER_TWO = 1;
+    private static final int PLAYER_THREE = 2;
+    private static final int PLAYER_FOUR = 3;
 
     private newGame game;
     private Hex[][] hexes;
@@ -39,29 +21,29 @@ public class NewView {
     public NewView(newGame game) {
         this.game = game;
         hexes = game.getBoard().getHexes();
-
     }
 
     public void update(Group root) {
-        // Draw vertices
         Hex hex;
         for (int i = 0; i < hexes.length; i++) {
             for (int j = 0; j < hexes.length; j++) {
                 hex = hexes[i][j];
                 if (hex != null) {
+                    // Draw vertices
                     drawVertices(root, hex);
                     drawPaths(root, hex);
+                    drawRobber(root);
 
 
                 }
-
-
             }
         }
+    }
 
-
-
-
+    private void drawRobber(Group root) {
+        Hex robberHex = game.getBoard().getCurrentRobberPosHex();
+        Circle circle = new Circle(robberHex.getRealX(), robberHex.getRealY(), robberHex.getSize()/4.);
+        root.getChildren().add(circle);
     }
 
     private void drawPaths(Group root, Hex hex) {
@@ -73,7 +55,7 @@ public class NewView {
                 continue;
             }
             Line line = new Line(points[k].getX(), points[k].getY(), points[(k+1)%6].getX(), points[(k+1)%6].getY());
-            line.setStrokeWidth(HEX_SIZE/10.);
+            line.setStrokeWidth(hex.getSize() / 10.);
             root.getChildren().add(line);
 
             Paint paint = getColorFromPlayerID(edges[k].getId());
@@ -123,44 +105,5 @@ public class NewView {
 
     private Point[] getAdjacentVerticesLocation(Hex hex) {
         return hex.getAdjacentVerticesLocation();
-    }
-
-    // Shifts y-coordinate in hex to x,y-basis
-    private double getHexCenterY(Hex hex) {
-        return hex.getRealY();
-    }
-
-    // Shifts x-coordinate in hex to x,y-basis
-    private double getHexCenterX(Hex hex) {
-        return hex.getRealX();
-    }
-
-    // Finds the correct color depending on the terrain
-    private Paint getColorFromTerrain(Terrain terrain) {
-        Paint color;
-
-        switch (terrain) {
-            case Fields:
-                color = Color.WHEAT;
-                break;
-            case Forest:
-                color = Color.FORESTGREEN;
-                break;
-            case Pasture:
-                color = Color.LAWNGREEN;
-                break;
-            case Hills:
-                color = Color.FIREBRICK;
-                break;
-            case Mountains:
-                color = Color.LIGHTGREY;
-                break;
-            case Desert:
-                color = Color.SANDYBROWN;
-                break;
-            default:
-                color = Color.BLACK;
-        }
-        return color;
     }
 }
